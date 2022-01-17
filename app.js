2cf0692957373f2dd9e254a64b7382b74f368601
Felipe Aravena <felipearavena98@hotmail.com>
2022-01-17 00:07:25 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,68 @@
+require('colors');
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const { 
+    inquirerMenu, 
+    pausa, 
+    leerInput,
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoChecklist
+} = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
+
+
+const main = async() => {
+    console.log('Hola Mundo!');
+
+    let opt = '';
+    const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+
+    if (tareasDB){ // Cargar tareas
+        tareas.cargarTareasFromArray(tareasDB);
+    }
+
+    do {
+        // Imprimir el menú
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripcion: ');
+                tareas.crearTarea(desc);
+            break;
+            case '2':
+                tareas.listadoCompleto();
+            break;
+            case '3': // Listar completadas
+                tareas.listarPendientesCompletadas(true);
+            break;
+            case '4': // Listar pendientes
+                tareas.listarPendientesCompletadas(false);
+            break;
+            case '5': // Completado | Pendiente
+                const ids = await mostrarListadoChecklist(tareas.listadoArr);
+                tareas.toggleCompletadas(ids);
+            break;
+            case '6': // borrar
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                if (id !== '0') {
+                    const ok = await confirmar('¿Está Seguro?');
+                    if (ok) {
+                        tareas.borrarTarea(id);
+                        console.log('Tarea borrada');
+                    }
+                }
+            break;
+
+        }
+
+        guardarDB(tareas.listadoArr);
+
+        await pausa();
+    } while ( opt !== '0');
+
+}
+
+main();
